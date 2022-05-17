d6d950c0f35f5b79fd76ff6b9994d63f376c507c
pkwesst <pkwesst@gmail.com>
2022-05-17 16:16:46 +0900

    Date

---
+++ js-todolist/js/main.js
@@ -0,0 +1,92 @@
+const inputText = document.querySelector(".input-text");
+const addButton = document.querySelector(".add-button");
+const list = document.querySelector(".list");
+
+const likeButtons = document.querySelectorAll(".like");
+
+// 날짜
+let today = new Date();
+let month = new Intl.DateTimeFormat("en-US", { month: "short" }).format(today);
+let date = today.getDate();
+
+// 요일
+let weekday = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let todayWeekday = weekday[today.getDay()];
+const day = date + " " + month + " • " + todayWeekday;
+
+const dateHeader = document.querySelector("li");
+dateHeader.innerText = day;
+
+const addItem = () => {
+  if (inputText.value.trim() == "") {
+    return;
+  }
+
+  // like
+  const like = document.createElement("span");
+  const likeIcon = document.createElement("i");
+  like.classList.add("like");
+  likeIcon.classList.add("material-icons");
+  likeIcon.innerText = "favorite_border";
+  like.appendChild(likeIcon);
+
+  // item
+  const item = document.createElement("span");
+  item.classList.add("item");
+  item.innerText = inputText.value;
+
+  // manage
+  const manage = document.createElement("sapn");
+  const checkIcon = document.createElement("i");
+  const clearIcon = document.createElement("i");
+  checkIcon.classList.add("material-icons", "check");
+  clearIcon.classList.add("material-icons", "clear");
+  checkIcon.innerText = "check";
+  clearIcon.innerText = "clear";
+  manage.classList.add("manage");
+  manage.appendChild(checkIcon);
+  manage.appendChild(clearIcon);
+
+  const li = document.createElement("li");
+
+  // evnet
+  like.addEventListener("click", (e) => {
+    const target = e.target;
+    const text =
+      target.innerText === "favorite" ? "favorite_border" : "favorite";
+    target.innerText = text;
+  });
+  checkIcon.addEventListener("click", (e) => {
+    const target = e.target.parentNode.parentNode;
+    target.classList.add("done");
+  });
+  clearIcon.addEventListener("click", (e) => {
+    const target = e.target.parentNode.parentNode;
+    list.removeChild(target);
+  });
+
+  li.appendChild(like);
+  li.appendChild(item);
+  li.appendChild(manage);
+  list.appendChild(li);
+
+  // reset
+  inputText.value = "";
+  inputText.focus();
+};
+
+inputText.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    addItem();
+  }
+});
+
+addButton.addEventListener("click", addItem);
